e33af306b75d1303e0fcdbd27235a848e496a826
Duque881 <duque881@hotmail.com>
2026-04-23 23:14:54 -0300

    Update salvar_sqlite.py

--- src/salvar_sqlite.py
+++ src/salvar_sqlite.py
@@ -2,11 +2,13 @@ import pandas as pd
 import sqlite3
 
 df = pd.read_csv("dados/titanic_tratado.csv")
-
 conn = sqlite3.connect("dados/titanic.db")
 
-df.to_sql("passageiros", conn, if_exists="replace", index=False)
+df.to_sql("titanic", conn, if_exists="replace", index=False)
+
+teste = pd.read_sql("SELECT * FROM titanic LIMIT 5", conn)
+print(teste)
 
 conn.close()
 
-print("Banco de dados criado com sucesso em dados/titanic.db")
+print("Banco SQLite criado com sucesso!")
