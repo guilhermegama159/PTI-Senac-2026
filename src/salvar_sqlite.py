34d37f102d4bbe3f17d460ee069add8e7cfa18ca
Duque881 <duque881@hotmail.com>
2026-04-23 21:58:44 -0300

    Update salvar_sqlite.py

--- src/salvar_sqlite.py
+++ src/salvar_sqlite.py
@@ -1,1 +1,12 @@
-# Arquivo responsável por salvar os dados tratados em um banco SQLite
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('titanic.db')
+
+df = pd.read_csv('dados/titanic_tratado.csv')
+
+df.to_sql('passageiros', conn, if_exists='replace', index=False)
+
+conn.close()
+
+print("Banco SQLite criado com sucesso!")
