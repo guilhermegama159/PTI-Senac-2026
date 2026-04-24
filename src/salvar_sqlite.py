0eafaee7173be2f9ff4b01348f6e818c2914bc90
Duque881 <duque881@hotmail.com>
2026-04-23 23:13:12 -0300

    Update salvar_sqlite.py

--- src/salvar_sqlite.py
+++ src/salvar_sqlite.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import sqlite3
 
+df = pd.read_csv("dados/titanic_tratado.csv")
+
 conn = sqlite3.connect("dados/titanic.db")
 
-df = pd.read_csv('dados/titanic_tratado.csv')
-
-df.to_sql('passageiros', conn, if_exists='replace', index=False)
+df.to_sql("passageiros", conn, if_exists="replace", index=False)
 
 conn.close()
 
-print("Banco SQLite criado com sucesso!")
+print("Banco de dados criado com sucesso em dados/titanic.db")
